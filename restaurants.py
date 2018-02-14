402e195792fb6f9c8047d9d684c772c9c6a587bb
Khyber Sen <kkysen@gmail.com>
2018-02-14 18:21:20 -0500

    fixed the zipcode query (zipcode was not a top-level field; it was under address.zipcode)

--- restaurants.py
+++ restaurants.py
@@ -1,24 +1,67 @@
 from __future__ import print_function
 
 from pymongo import MongoClient
-
-client = MongoClient("lisa.stuy.edu")
-db = client.test
-restaurants = db.restaurants
-
-
-def restaurants_by_borough(borough):
-    return restaurants.find(dict(borough=borough))
+from pymongo.collection import Collection
+from pymongo.cursor import Cursor
+from pymongo.database import Database
 
 
-def restaurants_by_zipcode(zipcode):
-    return restaurants.find(dict(zipcode=zipcode))
+class Restaurants(object):
+    
+    def __init__(self,
+                 inet_address='lisa.stuy.edu',
+                 db_name='test',
+                 collection_name='restaurants'
+                 ):
+        # type: (str, str, str) -> None
+        self.client = MongoClient(inet_address)  # type: MongoClient
+        self.db = self.client[db_name]  # type: Database
+        self.restaurants = self.db[collection_name]  # type: Collection
+    
+    def count(self):
+        # type: () -> int
+        return self.restaurants.count()
+    
+    def by_borough(self, borough):
+        # type: (str) -> Cursor
+        return self.restaurants.find({'borough': borough})
+    
+    def by_zipcode(self, zipcode):
+        # type: (str) -> Cursor
+        return self.restaurants.find({'address.zipcode': zipcode})
+    
+    def by_borough_and_zipcode(self, borough, zipcode):
+        # type: (str, str) -> Cursor
+        return self.restaurants.find({
+            '$and': [
+                {'borough': borough},
+                {'address.zipcode': zipcode},
+            ]
+        })
+    
+    def close(self):
+        # type: () -> None
+        self.client.close()
+    
+    def __enter__(self):
+        # type: () -> Restaurants
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # type: () -> None
+        self.close()
 
 
-def restaurants_by_borough_and_zipcode(borough, zipcode):
-    q = {"$and": [dict(borough=borough), dict(zipcode=zipcode)]}
-    return restaurants.find(q)
-
-
-map(print, restaurants_by_borough("Brooklyn"))
-map(print, restaurants_by_borough_and_zipcode("Brooklyn", "11209"))
+if __name__ == '__main__':
+    restaurants = Restaurants()  # type: Restaurants
+    print(restaurants.count())
+    print()
+    # map(pprint,
+    #     islice((restaurant['address'].get('zipcode', None) for restaurant in
+    #             restaurants.by_borough("Brooklyn")), 0, 10))
+    map(print, restaurants.by_borough('Brooklyn').limit(2))
+    print()
+    map(print, restaurants.by_zipcode('11215').limit(2))
+    print()
+    map(print, restaurants.by_borough_and_zipcode('Brooklyn', '11209').limit(2))
+    restaurants.close()
